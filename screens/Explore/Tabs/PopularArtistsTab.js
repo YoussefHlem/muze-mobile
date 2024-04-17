f9e8c0adecdcf05daa02d478aab50c5c33b734bf
YoussefHlem <=>
2024-04-17 09:48:31 +0200

    fix unhandled promise

--- screens/Explore/Tabs/PopularArtistsTab.js
+++ screens/Explore/Tabs/PopularArtistsTab.js
@@ -2,22 +2,29 @@ import { useEffect, useState } from "react";
 import { View, StyleSheet, FlatList } from "react-native";
 import { getAllArtists } from "../../../apis/search";
 import ArtistCard from "../../../components/common/ArtistCard";
+import { getAuthToken } from "../../../utils/AuthToken";
 
 const PopularArtistsTab = () => {
   const [Artists, setArtists] = useState([]);
+  const [authToken, setAuthToken] = useState(null);
 
   useEffect(() => {
-    getAllArtists().then((res) => {
-      setArtists(res.data);
-    });
+    (async () => {
+      const authToken = await getAuthToken();
+      setAuthToken(authToken);
+    })();
+
+    if (authToken) {
+      getAllArtists().then((res) => {
+        setArtists(res.data);
+      });
+    }
   }, []);
   return (
     <View style={styles.container}>
       <FlatList
         data={Artists}
-        renderItem={({ item }) => (
-          <ArtistCard artist={item} id={item.user} key={item.user} />
-        )}
+        renderItem={({ item }) => <ArtistCard artist={item} id={item.user} key={item.user} />}
         contentContainerStyle={styles.flatListContent}
       />
     </View>
